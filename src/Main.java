b16d63a2d7f63cd69977faef3d5422bc9f4a2e78
Lucas França <lucasfilipefn13@gmail.com>
2026-01-12 19:04:08 -0300

    Departament Class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
+import model.Department;
 
 public class Main {
     public static void main(String[] args) {
-
+        
     }
 }
---
+++ src/model/Seller.java
@@ -0,0 +1,72 @@
+package model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Seller implements Serializable {
+    private Integer id;
+    private String name;
+    private String email;
+    private Date birthDate;
+    private Double baseSalary;
+
+
+    public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.birthDate = birthDate;
+        this.baseSalary = baseSalary;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(Double baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
+    @Override
+    public String toString() {
+        return "Seller{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", birthDate=" + birthDate +
+                ", baseSalary=" + baseSalary +
+                '}';
+    }
+}
