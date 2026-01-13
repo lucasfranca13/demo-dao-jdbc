da2d95e7b3c924360f912530f308d165dafdc592
Lucas França <lucasfilipefn13@gmail.com>
2026-01-13 18:55:43 -0300

    implement findAll

--- src/Main.java
+++ src/Main.java
@@ -18,5 +18,11 @@ public class Main {
         for (Seller obj : list){
             System.out.println(obj);
         }
+
+        System.out.println("\n==== TEST 3: seller findAll ====");
+        List<Seller> listAll = sellerDao.findAll();
+        for (Seller obj : listAll){
+            System.out.println(obj);
+        }
     }
 }
